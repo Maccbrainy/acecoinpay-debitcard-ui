8c997235a6dcf912526fef0011adb995daec08cd
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-06-01 15:56:46 +0100

    removed dublicate icon

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className='min-h-screen flex items-center justify-center overflow-x-auto px-4'>
       <div className="bg-[url('/src/assets/payment-checkout-bg.svg')] object-contain bg-no-repeat w-full max-w-[1000px] h-full lg:h-[100vh] text-[#202951] lg:mt-12 lg:mb-10 lg:border relative">
-        <MdClear className="w-5 h-5 right-0 absolute border text-[#202951]" />
           <div className='w-full h-full bg-[#ffffffc7] backdrop-blur'>
             <MdClear fontSize={35} className="p-[9.5px] bg-[#f8f8f8] right-0 absolute  text-[#202951] cursor-pointer" />
             <Header/>
